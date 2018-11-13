8a8b4a4dab74da9876799e53b1ed2245fa61f227
Joe Ho <joebcrmd@gmail.com>
2018-11-13 18:46:10 +0800

    added hero image and PT narrow sans font

--- src/script.js
+++ src/script.js
@@ -9,6 +9,11 @@
       mobileToggle.classList.toggle("mobile__toggle--active");
       listToggle.classList.toggle("nav__list--active");
     }
+
+    if (event.target.matches(".item__link")){
+      mobileToggle.classList.remove("mobile__toggle--active");
+      listToggle.classList.remove("nav__list--active");
+    }
    }
 
   document.addEventListener("click", handleToggle); 
