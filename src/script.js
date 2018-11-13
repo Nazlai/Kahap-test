c0ae8e2ef2b0b8b9ddd29d955e8e3f2c64780532
Joe Ho <joebcrmd@gmail.com>
2018-11-13 15:30:37 +0800

    added dropdown navbar styling

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
 (function(){
   'use strict';
-
+  
   const handleToggle = function handleToggle(event){
     const mobileToggle = document.getElementById("js-toggle");
+    const listToggle = document.getElementById("js-list");
 
     if (event.target.closest("#js-hamburger")){
       mobileToggle.classList.toggle("mobile__toggle--active");
+      listToggle.classList.toggle("nav__list--active");
     }
    }
 
